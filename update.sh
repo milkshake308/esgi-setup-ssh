02af16f9b512460f4eceaeaf0a2399958909d82c
Aaron S <rnsaadoun@hotmail.com>
2022-12-06 12:48:27 +0100

    add banner support

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 cp config/sshd_config /etc/ssh/sshd_config
+cp config/banner /etc/ssh/banner
 cp config/authorized_keys /home/aaron/.ssh/authorized_keys
 chown -R aaron:aaron /home/aaron/.ssh/authorized_keys
 systemctl reload ssh.service
